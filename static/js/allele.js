87856b29cead98c24a18931efe216a9cc3010a5b
Robert Ashton <r.ashton@imperial.ac.uk>
2022-02-11 22:51:17 +0000

    First cut of site

---
+++ static/js/allele.js
@@ -0,0 +1,35 @@
+function myFunction(x) {
+  let d = new Date();
+  alert("Current datetime: " + d + "\nYou passed in: " + x);
+}
+
+function getGeneration(p, n) {
+  console.log(p.value);
+  console.log(n.value);
+  var result = {
+    redRed: 0,
+    redYellow: 0,
+    yellowRed: 0,
+    yellowYellow: 0
+
+};
+  for (var i = 0; i < n.value; i++) {
+    var first = draw(p.value);
+    var second = draw(p.value);
+    if (first && second) {
+      result.redRed++;
+    } else if (first && !second) {
+      result.redYellow++;
+    } else if (!first && second) {
+      result.yellowRed++;
+    } else if (!first && !second) {
+      result.yellowYellow++;
+    }
+  }
+  alert("You got " + result.redRed + " and " + result.redYellow + ", " + result.yellowRed + ", " + result.yellowYellow);
+}
+
+// Model p as prob of red q as prob of yellow
+function draw(p) {
+  return Math.random() < p;
+}
