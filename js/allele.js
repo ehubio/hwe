f56a816c1913e15b04982e9e80a78d4f5a0fa834
r-ash <r-ash@users.noreply.github.com>
2022-02-13 23:31:26 +0000

    deploy: 925910e3889142995390474eead2e01b1eb116a2

--- js/allele.js
+++ js/allele.js
@@ -3,9 +3,10 @@ function myFunction(x) {
   alert("Current datetime: " + d + "\nYou passed in: " + x);
 }
 
-function getGeneration(p, n) {
-  console.log(p.value);
-  console.log(n.value);
+function getGeneration(p, q, n) {
+  if (!input_valid(Number(p.value), Number(q.value))) {
+    return;
+  }
   var result = {
     redRed: 0,
     redYellow: 0,
@@ -14,8 +15,8 @@ function getGeneration(p, n) {
 
 };
   for (var i = 0; i < n.value; i++) {
-    var first = draw(p.value);
-    var second = draw(p.value);
+    var first = draw(Number(p.value));
+    var second = draw(Number(p.value));
     if (first && second) {
       result.redRed++;
     } else if (first || second) {
@@ -25,12 +26,21 @@ function getGeneration(p, n) {
     }
   }
   // There is probably a smarter way to do this
-  rROutput = document.querySelector('#rROutput');
-  rYOutput = document.querySelector('#rYOutput');
-  yYOutput = document.querySelector('#yYOutput');
+  var rROutput = document.querySelector('#rROutput');
+  var rYOutput = document.querySelector('#rYOutput');
+  var yYOutput = document.querySelector('#yYOutput');
   rROutput.textContent = result.redRed;
   rYOutput.textContent = result.redYellow;
   yYOutput.textContent = result.yellowYellow;
+  return;
+}
+
+function input_valid(p, q) {
+  var valid = p + q == 1.00;
+  if (!valid) {
+    document.getElementById("imsg").innerHTML = "p and q must add up to 1";
+  }
+  return valid;
 }
 
 // Model p as prob of red q as prob of yellow
